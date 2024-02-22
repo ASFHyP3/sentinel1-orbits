7060ebff388219d71dd2dc753ded19ae6b868d00
Andrew Johnston <asjohnston@alaska.edu>
2024-02-21 19:53:31 -0900

    process one orbit type per run and add s3 lifecycle policy

--- fetcher/fetcher.py
+++ fetcher/fetcher.py
@@ -1,6 +1,5 @@
 import json
 import os
-import time
 
 import boto3
 import requests
@@ -49,70 +48,65 @@ class EsaToken:
         response.raise_for_status()
 
 
-def get_s3_orbits(bucket_name: str) -> set[str]:
+def get_s3_orbits(bucket_name: str, prefix: str) -> set[str]:
+    paginator = s3.get_paginator('list_objects_v2')
     params = {
         'Bucket': bucket_name,
+        'Prefix': prefix,
     }
     objects = []
-    while True:
-        response = s3.list_objects_v2(**params)
-        objects.extend(response['Contents'])
-        if 'NextContinuationToken' not in response:
-            break
-        params['ContinuationToken'] = response['NextContinuationToken']
+    for page in paginator.paginate(**params):
+        if 'Contents' in page:
+            objects.extend(page['Contents'])
     return {os.path.basename(obj['Key']) for obj in objects}
 
 
-def get_cdse_orbits() -> list[dict]:
+def get_cdse_orbits(orbit_type) -> list[dict]:
     url = 'https://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel1/search.json'
     cdse_orbits = []
 
-    for product_type in ['AUX_POEORB', 'AUX_RESORB']:
-        params = {
-            'productType': product_type,
-            'maxRecords': 1000,
-            'page': 1,
-        }
-        items = True
-        while items:
-            response = session.get(url, params=params)
-            response.raise_for_status()
-            items = [{'filename': feature['properties']['title'], 'id': feature['id']} for feature in response.json()['features']]
-            cdse_orbits.extend(items)
-            params['page'] += 1
+    params = {
+        'productType': orbit_type,
+        'maxRecords': 1000,
+        'page': 1,
+    }
+    items = True
+    while items:
+        response = session.get(url, params=params)
+        response.raise_for_status()
+        items = [{'filename': feature['properties']['title'], 'id': feature['id']} for feature in response.json()['features']]
+        cdse_orbits.extend(items)
+        params['page'] += 1
     return cdse_orbits
 
 
-def copy_file(filename: str, file_id: str, token: str, bucket_name: str) -> None:
+def copy_file(filename: str, file_id: str, token: str, bucket_name: str, orbit_type: str) -> None:
     headers = {'Authorization': f'Bearer {token}'}
     url = f'https://zipper.dataspace.copernicus.eu/download/{file_id}'
     response = session.get(url, headers=headers)
     response.raise_for_status()
-    if 'AUX_POEORB' in filename:
-        key = f'AUX_POEORB/{filename}'
-    else:
-        key = f'AUX_RESORB/{filename}'
-    s3.put_object(Bucket=bucket_name, Key=key, Body=response.text)
+
+    s3.put_object(Bucket=bucket_name, Key=f'{orbit_type}/{filename}', Body=response.text)
 
 
 def lambda_handler(event, context):
     secret_arn = os.environ['SECRET_ARN']
     bucket_name = os.environ['BUCKET_NAME']
+    orbit_type = event['orbit_type']
 
     response = secretsmanager.get_secret_value(SecretId=secret_arn)
     credentials = json.loads(response['SecretString'])
 
-    print('Getting S3 inventory')
-    s3_orbits = get_s3_orbits(bucket_name=bucket_name)
+    print(f'Getting S3 {orbit_type} inventory')
+    s3_orbits = get_s3_orbits(bucket_name=bucket_name, prefix=orbit_type)
 
-    print('Getting CDSE inventory')
-    cdse_orbits = get_cdse_orbits()
+    print(f'Getting CDSE {orbit_type} inventory')
+    cdse_orbits = get_cdse_orbits(orbit_type=orbit_type)
 
     orbits_to_copy = [orbit for orbit in cdse_orbits if orbit['filename'] not in s3_orbits]
-    print(f'Found {len(orbits_to_copy)} orbit files to fetch')
+    print(f'Found {len(orbits_to_copy)} {orbit_type} orbit files to fetch')
 
     with EsaToken(username=credentials['username'], password=credentials['password']) as token:
         for orbit in orbits_to_copy:
             print(f'Fetching {orbit["filename"]}')
-            copy_file(orbit['filename'], orbit['id'], token, bucket_name)
-            time.sleep(15)
+            copy_file(orbit['filename'], orbit['id'], token, bucket_name, orbit_type)
