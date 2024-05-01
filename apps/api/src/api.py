fee38b29bd44d657b2a5619d31308308a7678cc1
Andrew Johnston <asjohnston@alaska.edu>
2024-05-01 15:31:54 -0800

    refactor api

--- apps/api/src/api.py
+++ apps/api/src/api.py
@@ -6,23 +6,32 @@ import boto3
 s3 = boto3.client('s3')
 
 
+def build_url(bucket: str, key: str) -> str:
+    return f'https://{bucket}.s3.amazonaws.com/{key}'
+
+
+def list_bucket(bucket: str, prefix: str) -> list[str]:
+    paginator = s3.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(
+        Bucket=bucket,
+        Prefix=prefix,
+    )
+    return [item['Key'] for page in page_iterator for item in page.get('Contents', [])]
+
+
 def get_orbit_for_granule(granule: str, bucket: str, orbit_type: str):
     platform = granule[0:3]
     start_date = granule[17:32]
     end_date = granule[33:48]
 
-    paginator = s3.get_paginator('list_objects_v2')
-    page_iterator = paginator.paginate(
-        Bucket=bucket,
-        Prefix=f'{orbit_type}/{platform}',
-    )
-    for page in page_iterator:
-        for item in page.get('Contents', []):
-            filename = os.path.basename(item['Key'])
-            start = filename[42:57]
-            end = filename[58:73]
-            if start <= start_date <= end_date <= end:
-                return item['Key']
+    keys = list_bucket(bucket, prefix=f'{orbit_type}/{platform}')
+    keys.sort(reverse=True)
+    for key in keys:
+        filename = os.path.basename(key)
+        start = filename[42:57]
+        end = filename[58:73]
+        if start <= start_date <= end_date <= end:
+            return key
     return None
 
 
@@ -30,7 +39,7 @@ def get_url(granule, bucket):
     for orbit_type in ['AUX_POEORB', 'AUX_RESORB', 'AUX_PREORB']:
         key = get_orbit_for_granule(granule, bucket, orbit_type)
         if key:
-            return f'https://{bucket}.s3.amazonaws.com/{key}'
+            return build_url(bucket, key)
     return None
 
 
