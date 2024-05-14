4d2b486af3447faafd68c1448a162827e4d7a631
Andrew Johnston <asjohnston@alaska.edu>
2024-05-14 15:30:16 -0800

    test api.list_bucket

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,14 @@
+import pytest
+from botocore.stub import Stubber
+
 import api
+
+
+@pytest.fixture
+def s3_stubber():
+    with Stubber(api.s3) as stubber:
+        yield stubber
+        stubber.assert_no_pending_responses()
 
 
 def test_lambda_handler():
@@ -27,3 +37,40 @@ def test_lambda_handler():
 def test_build_url():
     assert api.build_url('foo', 'bar') == 'https://foo.s3.amazonaws.com/bar'
     assert api.build_url('bucket', 'key/with/prefix.txt') == 'https://bucket.s3.amazonaws.com/key/with/prefix.txt'
+
+
+def test_list_bucket(s3_stubber):
+    expected_params = {
+        'Bucket': 'foo',
+        'Prefix': 'bar'
+    }
+    service_response = {
+        'Contents': [
+            {'Key': 'a'},
+            {'Key': 'e'},
+            {'Key': 'b'},
+        ],
+        'IsTruncated': True,
+        'NextContinuationToken': 'token',
+    }
+    s3_stubber.add_response(method='list_objects_v2', expected_params=expected_params,
+                            service_response=service_response)
+
+    expected_params = {
+        'Bucket': 'foo',
+        'Prefix': 'bar',
+        'ContinuationToken': 'token',
+    }
+    service_response = {
+        'Contents': [
+            {'Key': 'c'},
+            {'Key': 'f'},
+        ],
+    }
+    s3_stubber.add_response(method='list_objects_v2', expected_params=expected_params,
+                            service_response=service_response)
+
+    assert api.list_bucket('foo', 'bar') == ['f', 'e', 'c', 'b', 'a']
+
+    # test cached response is returned instead of making more requests to S3
+    assert api.list_bucket('foo', 'bar') == ['f', 'e', 'c', 'b', 'a']
