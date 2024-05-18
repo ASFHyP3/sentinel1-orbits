862f589e5af13e7524610d0279e8e67d55a5f4ee
Andrew Johnston <asjohnston@alaska.edu>
2024-05-17 16:00:20 -0800

    test fixes

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from botocore.stub import Stubber
-
-
-@pytest.fixture
-def s3_stubber():
-    with Stubber(api.s3) as stubber:
-        yield stubber
-        stubber.assert_no_pending_responses()
